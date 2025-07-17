74d7c5b3064248e0c6072aa7f1d59bf13e61d43b
ZiglaCity <ziglacity@gmail.com>
2025-07-17 02:25:16 +0000

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+     <p className='text-green-500 text-center'>Hello there, it's My Porfolio time!</p>
+    </>
+  )
+}
+
+export default App
